0f319f6508a6aedc3538fc918ca440a3b6f51f9c
eaimeric <aimeric.egal@ynov.com>
2026-02-15 00:10:35 +0100

    init code structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+function App() 
+{
+  //state
 
-function App() {
-  const [count, setCount] = useState(0)
+  
+  //behavior
 
+
+  //render
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <html>
+      <head>
+        <title>Responsive</title>
+      </head>
+      <body>
+        <div>
+          <h1>Accueil</h1>
+          <h1>Recherche</h1>
+          <h1>Users</h1>
+        </div>
+      </body>
+    </html>
+  );
 }
 
 export default App
---
+++ src/layout/Header.jsx
@@ -0,0 +1,13 @@
+function Header()  {
+  return (
+    <header>
+      <h1>Responsive App</h1>
+      <div>
+        <logo src="logo.png" alt="Logo" />
+        <h1>Accueil</h1>
+        <h1>Recherche</h1>
+        <h1>Users</h1>
+      </div>
+    </header>
+  )
+}
